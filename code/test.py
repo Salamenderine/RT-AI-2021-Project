68b34821bea0757d19115aee0e4e02b31449d663
1170801121 <396588650@qq.com>
2021-11-25 14:26:46 +0100

    up test

--- code/test.py
+++ code/test.py
@@ -4,9 +4,9 @@ files= os.listdir('../mnist_nets')
 for f in files:
     tests = os.listdir('../test_cases/' + f[:-3])
     for test in tests:
-        print(f,test)
+        print(f[:-3], test, sep=' ')
         os.system('python verifier.py --net ' + f[:-3] + ' --spec ' + os.path.join('..', 'test_cases', f[:-3], test))
-        
+
 # with open('test_command.txt') as f:
 #     for line in f.readlines():
 #         # print(line.split(''))
